d6e63e89410e874182912e566374e9407cfe0afb
Adrian Pavel <adrian199909@gmail.com>
2022-10-22 02:38:21 +0200

    Location endpoints

--- srcs/backend/routes/location.py
+++ srcs/backend/routes/location.py
@@ -2,58 +2,58 @@ import json
 from sqlalchemy.orm import Session
 from sqlalchemy import select
 from flask import jsonify
-from models import TagModel
+from models import LocationModel
 from flask import request
 from middlewares import auth_check
 
-class Tag:
+class Location:
 	def __init__(self, engine):
 		self.engine = engine
 
-	# /tag/
-	def get_all_tags(self):
+	# /location
+	def get_all_locations(self):
 		with Session(self.engine) as session:
-			statement = select(TagModel.id, TagModel.name)
+			statement = select(LocationModel.id, LocationModel.name)
 			result = session.execute(statement)
 			if result == None:
-				return jsonify({"error": "None tag exists"}), 403
-			tags = []
+				return jsonify({"error": "None location exists"}), 403
+			locations = []
 			for r in result:
 				row = {
 					"id": r.id,
 					"name": r.name
 				}
-				tags.append(row)
+				locations.append(row)
 
-			return jsonify(tags), 200
+			return jsonify(locations), 200
 
-	# /tag/
-	def create_tag(self):
+	# /location
+	def create_location(self):
 		auth = auth_check()
 		if auth == None:
 			return jsonify({}), 401
 
 		data = request.json
-		tag = TagModel(
+		location = LocationModel(
 			name = data["name"]
 		)
 
 		with Session(self.engine) as session:
-			statement = select(TagModel.id, TagModel.name).where(TagModel.name == data["name"])
+			statement = select(LocationModel.id, LocationModel.name).where(LocationModel.name == data["name"])
 			rows = session.execute(statement).fetchone()
 			if rows != None:
-				return jsonify({"error": "Tag already exists."}), 403
-			session.add(tag)
+				return jsonify({"error": "Location already exists."}), 403
+			session.add(location)
 			session.commit()
 			return jsonify({}), 200
 
-	# /tag/<id>
-	def get_tag(self, id):
+	# /location/<id>
+	def get_location(self, id):
 		with Session(self.engine) as session:
-			statement = select(TagModel.id, TagModel.name).where(TagModel.id == id)
+			statement = select(LocationModel.id, LocationModel.name).where(LocationModel.id == id)
 			result = session.execute(statement).fetchone()
 			if result == None:
-				return jsonify({"error": "None tag exists with the provided ID."}), 403
+				return jsonify({"error": "None location exists with the provided ID."}), 403
 			row = {
 				"id": result.id,
 				"name": result.name
